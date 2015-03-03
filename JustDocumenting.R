0635fe2d5174d574a61c20579d88bc2ed55d7351
SpartanNick <nicolas.lecontedemarsy@gmail.com>
2015-03-03 22:16:46 +0100

    Uploading Basic Documentation Script

---
+++ JustDocumenting.R
@@ -0,0 +1,10 @@
+# install package does NOT make it available directly
+install.packages(c("slidify", "ggplot2", "devtools", "KernSmooth"))
+
+# To make it available, you need to load it, using library, NO quotes needed...
+library(slidify)
+# somehow slidify was not available for R v3.1.2...
+
+# check if a package is available
+find.package("slidify")
+# Will return Error if does not exist...
