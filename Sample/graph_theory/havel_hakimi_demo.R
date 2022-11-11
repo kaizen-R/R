13ee94d090713178039af726c888111f10abfc0e
kaizen-R <11278891+kaizen-R@users.noreply.github.com>
2022-11-11 21:38:18 +0100

    Update havel_hakimi_demo.R
    error correction and more common examples used

--- Sample/graph_theory/havel_hakimi_demo.R
+++ Sample/graph_theory/havel_hakimi_demo.R
@@ -1,5 +1,5 @@
 # Example implementation of Havel-Hakimi algorithm
-# Based on Pseudo-code from "Graph Theory and its applicatioins", 3rd ed., J.L. Gross, J. Yellen, M. Anderson, Ed. CRC
+# Based on Pseudo-code from "Graph Theory and its applications", 3rd ed., J.L. Gross, J. Yellen, M. Anderson, Ed. CRC
 # The code is my own, and any error is my mistake.
 
 library(igraph) # here only for sample_degseq()
@@ -29,14 +29,14 @@ graphic_sequence <- function(my_seq, verbose = FALSE) {
 }
 
 # Testing it:
-test_degrees_seq <- c(6,6,6,6,4,4,4)
+test_degrees_seq <- c(6,3,3,3,3,2,2,2,2,1,1) # Example from Wikipedia
 graphic_sequence(test_degrees_seq, verbose = TRUE) # should return TRUE, as the sequence is "graphic"
 
 # Plotting
 # Supposing the sequence is OK, this function from the igraph library is handy:
 plot(sample_degseq(test_degrees_seq, method="vl")) 
 
-test_degrees_seq <- c(6,6,6,6,4,4)
+test_degrees_seq <- c(6,6,5,3,4,4)
 graphic_sequence(test_degrees_seq, verbose = TRUE) # should return FALSE, not enough vertices for the sequence
 test_degrees_seq <- c(6,6,6,6,4,4,3)
 graphic_sequence(test_degrees_seq) # should return FALSE, odd total for degrees
